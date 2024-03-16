1cc4d083c1de5109d05623f7c30af49bbb243924
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-17 02:16:45 +0300

    Update settings.py

--- asset_management/asset_management/settings.py
+++ asset_management/asset_management/settings.py
@@ -148,3 +148,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'inventory_management.CustomUser'  # Replace 'your_app' with the name of your Django app
