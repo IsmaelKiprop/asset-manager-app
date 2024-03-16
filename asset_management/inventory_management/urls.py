17d189e986eedc9a3dc0a856307c79d9a70fd5dc
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-16 15:37:10 +0300

    Update urls.py

--- asset_management/inventory_management/urls.py
+++ asset_management/inventory_management/urls.py
@@ -1,7 +1,10 @@
+# asset_management_backend/inventory_management/urls.py
+
 from django.urls import path
-from .views import login
+from .views import RegisterUser, LoginUser, LogoutUser
 
 urlpatterns = [
-    path('login/', login, name='login'),
-    # Add other URLs for your application here
+    path('auth/register/', RegisterUser.as_view(), name='register'),
+    path('auth/login/', LoginUser.as_view(), name='login'),
+    path('auth/logout/', LogoutUser.as_view(), name='logout'),
 ]
