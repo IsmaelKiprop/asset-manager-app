e6e159c298ec2d640675c444e7f428c5ab8d1ad4
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-16 15:27:06 +0300

    Update serializers.py

--- asset_management/inventory_management/serializers.py
+++ asset_management/inventory_management/serializers.py
@@ -1,3 +1,16 @@
 from rest_framework import serializers
+from .models import CustomUser
 
-# Define your serializers here
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
