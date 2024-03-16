876726b57f6a0b81ce3c746e7aeae8fa1c1398ef
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-16 19:56:20 +0300

    Update serializers.py

--- asset_management/inventory_management/serializers.py
+++ asset_management/inventory_management/serializers.py
@@ -66,3 +66,28 @@ class ContractSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contract
         fields = '__all__'
+
+class SavingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Savings
+        fields = '__all__'
+
+class BudgetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Budget
+        fields = '__all__'
+
+class BankIntegrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BankIntegration
+        fields = '__all__'
+
+class BillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bill
+        fields = '__all__'
+
+class FinancialReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FinancialReport
+        fields = '__all__'
