6d4fc0471ce3b8ed337a58826f17ef380bec85c6
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-16 15:28:14 +0300

    Update views.py

--- asset_management/inventory_management/views.py
+++ asset_management/inventory_management/views.py
@@ -1,26 +1,43 @@
-from rest_framework import status
+from rest_framework import generics, permissions
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
-from .serializers import TokenSerializer
+from knox.models import AuthToken
+from .models import CustomUser
+from .serializers import CustomUserSerializer
 
-@api_view(['POST'])
-def login(request):
-    """
-    API endpoint to authenticate users and generate tokens.
-    """
-    username = request.data.get('username')
-    password = request.data.get('password')
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = CustomUserSerializer
 
-    if not username or not password:
-        return Response({'error': 'Username and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        _, token = AuthToken.objects.create(user)
+        return Response({'user': CustomUserSerializer(user, context=self.get_serializer_context()).data,
+                         'token': token})
 
-    user = authenticate(username=username, password=password)
+class LoginAPI(generics.GenericAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = CustomUserSerializer
 
-    if user:
-        token, _ = Token.objects.get_or_create(user=user)
-        serializer = TokenSerializer(instance=token)  # Serialize the token
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
-        return Response({'error': 'Invalid username or password.'}, status=status.HTTP_401_UNAUTHORIZED)
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = CustomUser.objects.filter(email=email).first()
+
+        if user is None:
+            return Response({'error': 'Invalid credentials'}, status=400)
+
+        if not user.check_password(password):
+            return Response({'error': 'Invalid credentials'}, status=400)
+
+        _, token = AuthToken.objects.create(user)
+        return Response({'user': CustomUserSerializer(user, context=self.get_serializer_context()).data,
+                         'token': token})
+
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = CustomUserSerializer
+
+    def get_object(self):
+        return self.request.user
