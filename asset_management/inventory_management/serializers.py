4bcd4cb0924077518828312d8e9037123fb5bce8
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-16 18:13:07 +0300

    Update serializers.py

--- asset_management/inventory_management/serializers.py
+++ asset_management/inventory_management/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from .models import InventoryItem, Expense, IntangibleAsset, Machinery, HardwareSoftware, Furniture, Investment, FixedAsset, Contract
 
 CustomUser = get_user_model()
 
@@ -20,3 +21,48 @@ class CustomUserSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+
+class InventoryItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InventoryItem
+        fields = '__all__'
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expense
+        fields = '__all__'
+
+class IntangibleAssetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IntangibleAsset
+        fields = '__all__'
+
+class MachinerySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Machinery
+        fields = '__all__'
+
+class HardwareSoftwareSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HardwareSoftware
+        fields = '__all__'
+
+class FurnitureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Furniture
+        fields = '__all__'
+
+class InvestmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Investment
+        fields = '__all__'
+
+class FixedAssetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FixedAsset
+        fields = '__all__'
+
+class ContractSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contract
+        fields = '__all__'
