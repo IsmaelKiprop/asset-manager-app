0e0ec3604c9a89cf36cb996e94d67c26ddd42906
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-16 14:55:54 +0300

    Update views.py

--- asset_management/inventory_management/views.py
+++ asset_management/inventory_management/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from .serializers import TokenSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def login(request):
+    """
+    API endpoint to authenticate users and generate tokens.
+    """
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if not username or not password:
+        return Response({'error': 'Username and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = authenticate(username=username, password=password)
+
+    if user:
+        token, _ = Token.objects.get_or_create(user=user)
+        serializer = TokenSerializer(instance=token)  # Serialize the token
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        return Response({'error': 'Invalid username or password.'}, status=status.HTTP_401_UNAUTHORIZED)
