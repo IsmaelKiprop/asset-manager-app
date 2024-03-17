aba1b889c1907fd0a9870dc2c96361084e8d86df
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-17 07:47:38 +0300

    Update models.py

--- asset_management/inventory_management/models.py
+++ asset_management/inventory_management/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
 import binascii
 import os
 
