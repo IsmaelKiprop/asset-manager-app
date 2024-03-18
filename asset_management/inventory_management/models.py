85c7736b1fe7fd3df49fd3b57f78d2f594e92359
mwigoti <henrymwoha02@gmail.com>
2024-03-18 15:20:10 -0500

    corrections

--- asset_management/inventory_management/models.py
+++ asset_management/inventory_management/models.py
@@ -1,3 +1,11 @@
+#TO-DO
+
+#Create class Asset and reconstruct model
+#Reconstruct User and CustomUser
+
+
+
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
