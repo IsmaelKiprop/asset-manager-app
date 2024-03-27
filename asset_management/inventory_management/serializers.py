5070b901b601386fcf62b7f8796d4e52a521d29b
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-28 00:22:18 +0300

    Update serializers.py

--- asset_management/inventory_management/serializers.py
+++ asset_management/inventory_management/serializers.py
@@ -4,7 +4,6 @@ from rest_framework import serializers
 from .models import CustomUser, InventoryItem, Expense, IntangibleAsset, Machinery, ComputerHardware, ComputerSoftware, Furniture, Investment, FixedAsset, Contract, Savings, Budget, BankIntegration, Bill, FinancialGrowth
 
 class CustomUserSerializer(serializers.ModelSerializer):
-class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'email', 'company_name', 'location', 'number_of_employees', 'industry', 'first_name', 'last_name', 'password']
